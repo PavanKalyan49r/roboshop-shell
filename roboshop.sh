42959f6154084b95b6a9ef5d36329f212f41222a
PavanKalyan49r <pavan49r@gmail.com>
2024-01-12 13:59:29 +0530

    roboshop shellscript

--- roboshop.sh
+++ roboshop.sh
@@ -15,8 +15,8 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-03f8f4d516bfc9736 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instance[0].PrivateIpAddress' --output text)
-  echo "$i:$IP_ADDRESS"
+IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-03f8f4d516bfc9736 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+  echo "$i: $IP_ADDRESS"
 
 #CREATE R53 RECORD, MAKE SURE YOU DELETE EXISTING RECORD
 aws route53 change-resource-record-sets \
