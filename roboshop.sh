66d4532636a0d8e340cd3fee6704aa64dd0edf03
PavanKalyan49r <pavan49r@gmail.com>
2024-01-12 13:48:28 +0530

    roboshop shellscript

--- roboshop.sh
+++ roboshop.sh
@@ -8,11 +8,11 @@ DOMAIN_NAME="pavanaws.online"
 
 for -i in "${INSTANCES[@]}"
 do
-  if[ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-  then
-    INSTANCE_TYPE="t3.small"
-  else
-    INSTANCE_TYPE="t2.micro"
+if[ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+then
+INSTANCE_TYPE="t3.small"
+else
+INSTANCE_TYPE="t2.micro"
 fi
 
 IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-03f8f4d516bfc9736 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instance[0].PrivateIpAddress' --output text)
